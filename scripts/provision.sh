df351824a761ac5f368c93a224ca3378d2fe0591
Alvaro Miranda Aguilera <kikitux@gmail.com>
2018-10-30 15:31:06 +0100

    Update provision.sh

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -35,5 +35,6 @@ which terraform &>/dev/null || {
   pushd /usr/local/bin
   wget https://releases.hashicorp.com/terraform/0.11.10/terraform_0.11.10_linux_amd64.zip
   unzip terraform_0.11.10_linux_amd64.zip
+  rm terraform_0.11.10_linux_amd64.zip
   popd
 }
